a9c342bb0a8aabdc7b53d0018635514dd351703c
Dmitri <56200546+Decryot@users.noreply.github.com>
2020-07-10 20:11:35 -0400

    md5 hash printer

---
+++ hashloader.py
@@ -0,0 +1,11 @@
+import hashlib
+
+try:
+    file = open("passwords.txt","r")
+except:
+    print("file not found")
+    exit()
+    
+for password in file:
+    md5_hash = hashlib.md5(password.strip().encode('utf-8')).hexdigest()
+    print(md5_hash)
